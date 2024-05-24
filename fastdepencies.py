dd238fb5f6ba9e56ed4c966f45a9ccab9ad9ab7d
Nurudin Wahab Lartey <nurundin2010@gmail.com>
2024-05-24 03:49:55 +0000

    fast depencies

--- fastdepencies.py
+++ fastdepencies.py
@@ -1,22 +1,35 @@
-import unvicorn
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, Depends, HTTPException
 
-app= FastAPI()
+app = FastAPI()
 
-async def dependency(id:str, name: str, age:int):
-    return {"id":id, "name":name, "age":age}
-
-class dependency:
-    def __init__(self, id:str, name:str, age:int):
+class User:
+    def __init__(self, id: str, name: str, age: int):
         self.id = id
         self.name = name
         self.age = age
 
+def validate_user(user: User = Depends(User)):
+    if user.age < 18:
+        raise HTTPException(status_code=400, detail="You are not Eligible")
+    return user
+
 @app.get("/user/")
-async def user(dep:dependency = Depends(dependency)):
-    return dep
+async def get_user(user: User = Depends(validate_user)):
+    return user
+
+
+async def get_db():
+    db = DBSession()
+    try:
+        yield db
+    finally:
+        db.close()
+    
 
 @app.get("/admin/")
-async def admin(dep:dependency = Depends(dependency)):
-    return dep
+async def get_admin(user: User = Depends(validate_user)):
+    # Implement admin-specific logic here (e.g., authorization checks)
+    get_db()
+    return {"message": "Welcome, Admin!"}
 
